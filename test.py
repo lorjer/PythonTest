6fec1b61b01c1c9c50d366c2bc3fbf1ff39fff8a
lorjer <42957088+lorjer@users.noreply.github.com>
2018-09-04 15:49:42 +0800

    Initialization
    Initialization

---
+++ test.py
@@ -0,0 +1,10 @@
+#inputnum = int(input('请输入数字 :'))
+inputnum = int(65535)
+print("input number = ",inputnum,"\ncount 1024*768 = ",1024*768)
+
+
+print("This is sample for %%, str:%s, number:%d"%("字符测试",1024))
+
+s1 = 72
+s2 = 85
+print('去年成绩：%d,今年成绩：%d,今年提高的成绩百分比：%0.2f%%'%(s1,s2,(s2-s1)/s1*100))
